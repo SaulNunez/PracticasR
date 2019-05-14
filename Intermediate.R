c1dbf7368ed93e3b422aa18f9bc89a31b39cad82
Saul Nunez <saul.nunez99@gmail.com>
2019-05-14 14:29:35 -0600

    Intermediate

--- Intermediate.R
+++ Intermediate.R
@@ -1,1 +1,20 @@
-data(mtcars)
+for(i in 1:nrow(mtcars)){
+  if(mtcars[i,1] > 26){
+    print("This car is nice.");
+  }
+}
+
+#In a modern world where cars are considered gas guzzlers 
+#if they consume less than ~25ish MPG
+#Lets see how they fare
+if(mean(mtcars[,1]) < 25){
+  print("Gas guzzlers");
+}
+
+mpg_list <- list(mtcars[,1])
+#Returns a matrix of booleans denoting MPG values more or equal than 20
+mpg_acceptable = sapply(mpg_list, function(val){ val<20 })
+#Still a matrix, object couldn't be simplified
+class(mpg_acceptable)
+
+hist(mtcars[,1])
